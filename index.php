d0cdc03f67576e442cd8b77d3766fb27802aab92
Daniel Pires <danieldppires@gmail.com>
2021-02-10 11:53:46 -0300

    comando delete

--- index.php
+++ index.php
@@ -28,11 +28,14 @@
 	$aluno->insert();
 	echo $aluno;*/
 
-	$usuario = new Usuario();
-
+	//Alterar um usuário
+	/*usuario = new Usuario();
 	$usuario->loadById(8);
-
 	$usuario->update("professor", "!@#$%¨&*");
+	echo $usuario;*/
 
+	$usuario = new Usuario();
+	$usuario->loadById(7);
+	$usuario->delete();
 	echo $usuario;
 ?>
